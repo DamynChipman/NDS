e377166c40904be8b9f01c9b1f9b00259dbb7d51
Damyn Chipman <damyn.chipman@gmail.com>
2018-09-22 12:31:14 -0600

    Initial Commit

---
+++ Numerical Data Structures/main.cpp
@@ -0,0 +1,13 @@
+//   Created by Damyn Chipman on 9/22/18
+//   github: @camperD
+//   Copyright © 2018 Damyn Chipman. All rights reserved.
+//      FILE:   main.cpp
+//   PROJECT:   Numerical Data Structures
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
